5be00307cc6d2b0f05cf4b73aca152617bff89a6
Paulmburu <paulmburu53@gmail.com>
2017-02-27 21:30:55 +0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+operator=input("enter an operator")
+if operator is'+':
+    print(a+b)
+if operator is'-':
+    print(a-b)
+if operator is'*':
+    print(a*b)
+if operator is'/':
+    print(a/b)
+if operator is'**':
+    print(a**b)
